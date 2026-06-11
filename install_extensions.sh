2ff78a152cd92e7f9496f29a3bf8ace30ea2e368
kristbaum <max@kristenonline.de>
2026-06-11 11:05:54 +0200

    new dump

--- install_extensions.sh
+++ install_extensions.sh
@@ -19,7 +19,7 @@ fi
 echo ""
 echo "Downloading source data files..."
 mkdir -p sources
-BASE_URL="https://raw.githubusercontent.com/arthist-lmu/plafond3d/main/dumps/deckenmalerei.eu/2025_02"
+BASE_URL="https://raw.githubusercontent.com/arthist-lmu/plafond3d/main/dumps/deckenmalerei.eu/2026_04"
 curl -fsSL "$BASE_URL/entities.json"  -o sources/entities.json
 curl -fsSL "$BASE_URL/relations.json" -o sources/relations.json
 curl -fsSL "$BASE_URL/resources.json" -o sources/resources.json
